62feb6cfa76579865519b612968e77a38681d50b
IchBinJannick <jannick.medda@icloud.com>
2025-11-16 16:30:13 +0100

    Added Non Block Blocks (Door, Fences etc...)

--- src/main/java/net/jannick/tutorialmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/jannick/tutorialmod/datagen/ModRecipeProvider.java
@@ -52,5 +52,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .criterion(hasItem(ModBlocks.MAGIC_BLOCK), conditionsFromItem(ModBlocks.MAGIC_BLOCK))
                 .offerTo(recipeExporter, Identifier.of(TutorialMod.MOD_ID, "raw_pink_garnet_from_magic_block"));
 
+
+
     }
 }
