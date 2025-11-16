a060b6f54157144af3633abc128a867d740fc03a
IchBinJannick <jannick.medda@icloud.com>
2025-11-16 13:44:08 +0100

    Added Tooltips for Magic Block, Chisel and Cauliflower

--- src/main/java/net/jannick/tutorialmod/item/ModItems.java
+++ src/main/java/net/jannick/tutorialmod/item/ModItems.java
@@ -5,9 +5,14 @@ import net.jannick.tutorialmod.TutorialMod;
 import net.jannick.tutorialmod.item.costum.ChiselItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+
+import java.util.List;
 
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
@@ -15,7 +20,14 @@ public class ModItems {
     public static final Item CHISEL = registerItem("chisel", new ChiselItem(new Item.Settings().maxDamage(32)));
     public static final Item CAULIFLOWER = registerItem("cauliflower", new Item(new Item.Settings().food(
             ModFoodComponents.CAULIFLOWER
-    )));
+    )) {
+        @Override
+        public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+            tooltip.add(Text.translatable("tooltip.tutorialmod.cauliflower"));
+
+            super.appendTooltip(stack, context, tooltip, type);
+        }
+    });
     public static final Item STARLIGHT_ASHES = registerItem("starlight_ashes", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
