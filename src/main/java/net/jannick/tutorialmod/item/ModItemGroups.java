9897b3a01c759e86d12800197ae8751f69215f49
IchBinJannick <jannick.medda@icloud.com>
2025-11-16 13:00:36 +0100

    Added Costum Advanced Item ChiselItem

--- src/main/java/net/jannick/tutorialmod/item/ModItemGroups.java
+++ src/main/java/net/jannick/tutorialmod/item/ModItemGroups.java
@@ -20,6 +20,7 @@ public class ModItemGroups {
                     .entries((displayContext, entries) -> {
                         entries.add(ModItems.PINK_GARNET);
                         entries.add(ModItems.RAW_PINK_GARNET);
+                        entries.add(ModItems.CHISEL);
                     }).build());
 
     public static final ItemGroup TUTORIAL_MOD_BLOCKS_GROUP = Registry.register(Registries.ITEM_GROUP,
